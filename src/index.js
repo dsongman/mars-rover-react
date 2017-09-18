50979d8533e33ec1152db5053b6ee2b84b9cf1af
Danny Williams <danny.williams@chromeriver.com>
2017-09-17 17:06:59 -0700

    Use Ant styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+import 'antd/dist/antd.css'; // Load Ant Design CSS before Component CSS
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
