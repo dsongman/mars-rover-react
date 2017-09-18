f8e33883c902ce982e7958712de07a0b1b4d3cb5
Danny Williams <danny.williams@chromeriver.com>
2017-09-17 23:58:24 -0700

    Add Ant localization

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import {LocaleProvider} from 'antd';
+import enUS from 'antd/lib/locale-provider/en_US';
 
 class App extends Component {
   render() {
@@ -7,11 +9,13 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
+      <LocaleProvider locale={enUS}>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
+      </LocaleProvider>
     );
   }
 }
