91126e405e4f545e288547fb9e44015089659380
Danny Williams <danny.williams@chromeriver.com>
2017-09-20 03:04:43 -0700

    Add photos actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,6 @@
+import {NASA_API_KEY} from '../_private';
+export const REQUEST_DATE_FORMAT = 'YYYY-MM-DD';
+
 /**
  * @param {Moment} date 
  * @returns {Object}
@@ -19,5 +22,66 @@ export const setRover = rover => {
   return {
     type: SET_ROVER,
     rover
+  };
+};
+
+/**
+ * @param {String} rover 
+ * @param {String} date 
+ * @returns {Object}
+ */
+export const REQUEST_PHOTOS = 'REQUEST_PHOTOS';
+const requestPhotos = (rover, date) => {
+  return {
+    type: REQUEST_PHOTOS,
+    rover,
+    date
+  };
+};
+
+/**
+ * @param {String} rover 
+ * @param {String} date 
+ * @param {JSON} json 
+ * @returns {Object}
+ */
+export const RECEIVE_PHOTOS = 'RECEIVE_PHOTOS';
+const receivePhotos = (rover, date, json) => {
+  return {
+    type: RECEIVE_PHOTOS,
+    rover,
+    date,
+    json
+  };
+};
+
+const fetchPhotos = (rover, date) => {
+  let url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?earth_date=${date}&api_key=${NASA_API_KEY}`;
+  return dispatch => {
+    dispatch(requestPhotos(rover, date));
+    return fetch(url)
+      .then(response => response.json())
+      .then(json => dispatch(receivePhotos(rover, date, json)));
+  };
+};
+
+const shouldFetchPhotos = (state, rover, date) => {
+  const roverPhotos = state.photos;
+  let photoKey = rover + '-' + date;
+  const hasPhotosOnDate = Boolean(roverPhotos[photoKey]);
+  
+  return !(hasPhotosOnDate || roverPhotos.isFetching);
+};
+
+export const getPhotos = (rover, date) => {
+  let _rover = rover[0];
+  let _date = date.format(REQUEST_DATE_FORMAT);
+
+  return (dispatch, getState) => {
+    if (!shouldFetchPhotos(getState(), _rover, _date)) {
+      return Promise.resolve();
+    }
+
+    return dispatch(fetchPhotos(_rover, _date));
   };
 };
